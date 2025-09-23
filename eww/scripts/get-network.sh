c4e9594aa25fb679940342f5827f2129e3386ef2
Julliermy <vasconcelosjulliermy@gmail.com>
2025-09-23 16:34:45 -0300

    Correção do script de get-network do eww

--- eww/scripts/get-network.sh
+++ eww/scripts/get-network.sh
@@ -1,11 +1,18 @@
-CONNECTION_TYPE=$(nmcli -t -f TYPE,STATE device | grep -E "^ethernet:connected$|^wifi:connected$" | awk -F: '{print $1}')
+#!/bin/bash
 
-if [[ "$CONNECTION_TYPE" == "wifi" ]]; then
-    SSID=$(nmcli -t -f ACTIVE,SSID dev wifi | awk -F: '$1=="yes"{print $2}')
-    echo $SSID
-elif [[ "$CONNECTION_TYPE" == "ethernet" ]]; then
-    ETHERNET_NAME=$(nmcli -t -f NAME connection show --active | grep ethernet)
-    echo $ETHERNET_NAME
+# Pega o nome do Wi-Fi se a interface wireless estiver ativa
+SSID=$(nmcli -t -f ACTIVE,SSID dev wifi | awk -F: '$1=="sim"{print $2}')
+
+# Pega o nome da conexão ethernet se a interface estiver ativa
+ETHERNET_NAME=$(nmcli -t -f NAME,TYPE connection show --active | grep ethernet | awk '{print $1}')
+
+if [[ -n "$SSID" ]]; then
+  # Se o SSID não estiver vazio, significa que estamos conectados via Wi-Fi
+  echo "$SSID"
+elif [[ -n "$ETHERNET_NAME" ]]; then
+  # Se o nome da ethernet não estiver vazio, estamos conectados por cabo
+  echo "$ETHERNET_NAME"
 else
-    echo "None"
+  # Se ambos estiverem vazios, não há conexão de internet ativa
+  echo "None"
 fi
